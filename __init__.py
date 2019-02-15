31aaff5a5a5ff089b755c77eaab603b20dabc61e
Stephen Barnes <stephen.barnes.wex@gmail.com>
2019-02-15 16:15:55 +0000

    Update __init__.py
    Updated __init__.py to include each of the queries and responses for AHA.

--- __init__.py
+++ __init__.py
@@ -9,7 +9,34 @@ class HospitalAssistant(MycroftSkill):
     def handle_assistant_hospital(self, message):
         self.speak_dialog('assistant.hospital')
 
+    @intent_file_handler('whereami.intent')
+    def handle_whereami(self, message):
+        self.speak_dialog('where.am.i')
+
+    @intent_file_handler('diet.intent')
+    def handle_diet(self, message):
+        self.speak_dialog('diet.update')
+
+    @intent_file_handler('doctorvisit.intent')
+    def handle_doctorvisit(self, message):
+        self.speak_dialog('doctor.rounds.update')
+
+    @intent_file_handler('gohome.intent')
+    def handle_gohome(self, message):
+        self.speak_dialog('release.request')
+
+    @intent_file_handler('medication.intent')
+    def handle_medication(self, message):
+        self.speak_dialog('medication.update')
+
+    @intent_file_handler('thedoctoris.intent')
+    def handle_thedoctoris(self, message):
+        self.speak_dialog('who.is.the.doctor')
+
+    @intent_file_handler('thenurseis.intent')
+    def handle_thenurseis(self, message):
+        self.speak_dialog('who.is.the.nurse')
+
 
 def create_skill():
     return HospitalAssistant()
-
